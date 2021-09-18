9720a4a9f1757e1bf2519d73a1aaf4a0f6ac2040
berkanturkali <brktrkali@outlook.com>
2021-09-18 18:16:06 +0300

    add DescriptionFragment

--- app/src/main/java/com/arabam/android/assigment/ui/fragments/details/tabs/DescriptionFragment.kt
+++ app/src/main/java/com/arabam/android/assigment/ui/fragments/details/tabs/DescriptionFragment.kt
@@ -1,7 +1,29 @@
 package com.arabam.android.assigment.ui.fragments.details.tabs
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.arabam.android.assigment.R
+import com.arabam.android.assigment.data.model.DetailAdvert
+import com.arabam.android.assigment.databinding.FragmentDescriptionLayoutBinding
+import com.arabam.android.assigment.utils.Constants
 
-class DescriptionFragment:Fragment() {
+class DescriptionFragment : Fragment(R.layout.fragment_description_layout) {
+
+    private lateinit var binding: FragmentDescriptionLayoutBinding
+
+    private lateinit var mAdvert: DetailAdvert
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentDescriptionLayoutBinding.bind(view)
+        mAdvert = arguments?.getParcelable<DetailAdvert>(Constants.ADVERT_KEY)!!
+        binding.advert = mAdvert
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        binding.unbind()
+    }
+
 }
